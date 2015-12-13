c2fcf5ddba23c1a3d280448c87b7743a92bea020
Adrian Webb <adrian.webb@coraltech.net>
2015-12-13 01:09:12 -0500

    Adding a ubuntu configuration script and changing the ubuntu bootstrap script order.

---
+++ os/ubuntu/00_config.sh
@@ -0,0 +1,61 @@
+#
+# System settings
+#
+export DNS_IP=208.67.222.222
+export SYSTEM_TIMEZONE=EST
+export SYSTEM_LOCALE=en_US.UTF-8
+
+#
+# Base package versions
+#
+export BUILD_ESSENTIAL_VERSION=11.6ubuntu6
+export CMAKE_VERSION=2.8.12.2-0ubuntu3
+export RAKE_VERSION=10.0.4-1
+export UNZIP_VERSION=6.0-9ubuntu1.5
+export CURL_VERSION=7.35.0-1ubuntu2.5
+export ZLIBC_VERSION=0.9k-4.1
+export BISON_VERSION=2:3.0.2.dfsg-2
+export LLVM_VERSION=1:3.4-0ubuntu1
+export LLVM_DEV_VERSION=1:3.4-0ubuntu1
+export ZLIB1G_DEV_VERSION=1:1.2.8.dfsg-1ubuntu1
+export LIBPOPT_DEV_VERSION=1.16-8ubuntu1
+export LIBSSL_DEV_VERSION=1.0.1f-1ubuntu2.16
+export LIBCURL4_OPENSSL_DEV_VERSION=7.35.0-1ubuntu2.5
+export LIBXSLT1_DEV_VERSION=1.1.28-2build1
+export LIBYAML_DEV_VERSION=0.1.4-3ubuntu3.1
+export LIBPQ_DEV_VERSION=9.3.10-0ubuntu0.14.04
+export LIBREADLINE6_DEV_VERSION=6.3-4ubuntu2
+export LIBNCURSES5_DEV_VERSION=5.9+20140118-1ubuntu1
+export LIBEDITLINE_DEV_VERSION=1.12-6
+export LIBEDIT_DEV_VERSION=3.1-20130712-2
+
+#
+# Git configuration
+#
+export GIT_VERSION=1:1.9.1-1ubuntu0.1
+export GIT_USER=''
+export GIT_EMAIL=''
+
+#
+# Ruby configuration
+#
+export RUBY_DEV_VERSION=1:1.9.3.4
+export RVM_RUBY_VERSION=rbx-2.5.2
+
+export GEM_CM_DEV=''
+export GEM_CM_REVISION=0.1
+export GEM_CM_VERSION=0.1.3
+
+#
+# Command versions (only for testing purposes)
+#
+export MAKE_CMD_VERSION=3.81
+export CMAKE_CMD_VERSION=2.8.12.2
+export RAKE_CMD_VERSION=10.4.2
+export RUBY_CMD_VERSION=2.5.2
+export GEM_CMD_VERSION=2.4.8
+export OPENSSL_CMD_VERSION=1.0.1f
+export UNZIP_CMD_VERSION=6.00
+export CURL_CMD_VERSION=7.35.0
+export GIT_CMD_VERSION=1.9.1
+export CM_CMD_VERSION=0.1.3
