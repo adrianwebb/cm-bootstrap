8fe7daf44a06cc2de6733cafa3e8c68eaa26842b
Adrian Webb <adrian.webb@coraltech.net>
2015-12-13 01:56:35 -0500

    Fixing scoping issues with the test_command library function.

--- lib/test_command.sh
+++ lib/test_command.sh
@@ -8,7 +8,9 @@ function test_command {
   local version_options="${3:---version}"
 
   echo -n "it should expose the $name command... "
-   $name $version_options 2>&1 | grep -E "( |^)$version" > /dev/null
+  su - -c "
+  $name $version_options 2>&1 | grep -E '( |^)$version' > /dev/null
+  " root
   [ "$?" -ne 0 ] && echo fail && exit 1
   echo pass
 }
