11ac3be00287876fd6fc609ff46f32f22ccf8e58
Adrian Webb <adrian.webb@coraltech.net>
2015-12-26 19:34:57 -0500

    Using bundler and rake install to install development version of CM in the cm ubuntu bootstrap script.

--- os/ubuntu/tasks/80_cm.sh
+++ os/ubuntu/tasks/80_cm.sh
@@ -16,27 +16,28 @@ su - -c "gem uninstall cm -x --force" root || exit 101
 if [ "$GEM_CM_DEV" ]
 then
   rm -Rf /tmp/cm
+
+  echo "*** Installing Ruby Bundler library"  
+  su - -c "gem install bundler -v '$GEM_BUNDLER_VERSION'" root || exit 102
   
   if [ "$GEM_CM_DIRECTORY" ]
   then
     echo "*** Copying local CM gem directory"
-    cp -Rf "$GEM_CM_DIRECTORY" /tmp/cm || exit 102
+    cp -Rf "$GEM_CM_DIRECTORY" /tmp/cm || exit 103
     
     echo "*** Building CM gem from local copy"
   else
-    mkdir /tmp/cm || exit 102
-    cd /tmp/cm || exit 103
+    mkdir /tmp/cm || exit 103
+    cd /tmp/cm || exit 104
 
     echo "*** Fetching CM source repository"
-    git clone --branch "$GEM_CM_REVISION" git://github.com/adrianwebb/cm.git /tmp/cm || exit 104
-    git submodule update --init --recursive || exit 105
+    git clone --branch "$GEM_CM_REVISION" git://github.com/adrianwebb/cm.git /tmp/cm || exit 105
+    git submodule update --init --recursive || exit 106
     
     echo "*** Building CM gem from branch "$GEM_CM_REVISION""
   fi  
-  su - -c "cd /tmp/cm; gem build /tmp/cm/cm.gemspec" root || exit 106
-
-  echo "*** Installing development build of CM"
-  su - -c "cd /tmp/cm; gem install /tmp/cm/cm-*.gem" root || exit 107
+  su - -c "cd /tmp/cm; bundle install" root || exit 107
+  su - -c "cd /tmp/cm; bundle exec rake install" root || exit 108
 else
   echo "*** Installing latest release of CM"
   su - -c "gem install cm -v '$GEM_CM_VERSION'" root || exit 102
