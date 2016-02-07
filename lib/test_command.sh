fc5fccdf9cdbe76394b8dd86efb169fda8974bfd
Adrian Webb <adrian.webb@coraltech.net>
2016-02-07 04:21:54 -0500

    Updating the test_command and test_package functions to not check for specific versions.

--- lib/test_command.sh
+++ lib/test_command.sh
@@ -4,12 +4,11 @@
 #
 function test_command {
   local name=$1
-  local version=$2
   local version_options="${3:---version}"
 
   echo -n "it should expose the $name command... "
   su - -c "
-  $name $version_options 2>&1 | grep -E '( |^)$version' > /dev/null
+  $name $version_options 2>&1 | grep -P '( |^)[\d\.\_\-a-zA-Z]+' > /dev/null
   " root
   [ "$?" -ne 0 ] && echo fail && exit 1
   echo pass
